ef5e58511f7b9f6acd961200a5addb12a5fb273b
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-09-10 20:16:30 +0000

    si

---
+++ list-reference.py
@@ -0,0 +1,6 @@
+def main():
+    mylist = ["pencil", "computer", "shirt", "phone", "paper"]
+    print(len(mylist)) #len function works as a counter of how many things are in a list.
+
+if __name__ == "__main__":
+    main()
