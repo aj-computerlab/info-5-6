c577852aaa12a84174df55b5593c2986c1b117ab
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-09-17 17:20:33 +0000

    forintro

---
+++ for-intro.py
@@ -0,0 +1,8 @@
+def main():
+    doctrine = ["Faith", "Repent", "Baptism", "Holy Ghost", "Endure Till the End"]
+
+    for i in range(len(doctrine)):
+        print(doctrine[i])
+
+if __name__ == "__main__":
+    main()
--- to-do.py
+++ to-do.py
@@ -3,13 +3,16 @@ def main():
     while True:
         print(f"You have {len(tasks)} tasks to do.")
         print(tasks)
-        command = input("What do you want to do? (add, complete, or stop): ").lower()
+        command = input("What do you want to do? (add, complete, correct, or stop): ").lower()
         if command == "add":
             new_task = input("Enter a new task: ")
             tasks.append(new_task)
         elif command == "complete":
             outtask = input("Which task are you removing?: ")
             tasks.remove(outtask)
+        elif command == "correct":
+            rewrite = input("What task are you rewriting?: "))
+            command[rewrite] = command
         elif command == "stop":
             break
 
